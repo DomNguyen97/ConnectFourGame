d66fea35692540ccbe024415824dfc15e774dfd4
domwin <ndominic56@gmail.com>
2024-02-13 21:08:26 -0500

    render function cont

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 
 
 /*----- state variables -----*/
-
+let  board;  // array of 7 column arrays
+let turn; // 1 of -1 
+let winner; //null = no winner, 1 or -1 = winner; 'T' = tie game
 
 /*----- cached elements  -----*/
 
@@ -11,3 +13,48 @@
 
 
 /*----- functions -----*/
+init();
+// Initialize all sate, then call render()
+function init() {
+    //to visualize the board's mapping to the DOM,
+    // rotate the board's array 90 degrees counter-clockwise
+    board = [
+        [1, 0, 0, 0, 0, -1 ], //col 0
+        [0, 0, 0, 0, 0, 0 ], //col 1
+        [0, 0, 0, 0, 0, 0 ], //col 2
+        [-1, 0, 0, 0, 0, 0 ], //col 3
+        [0, 0, 0, 0, 0, 0 ], //col 4
+        [0, 0, 0, 0, 0, 0 ], //col 5
+        [0, 0, 0, 0, 0, 1 ], //col 6
+    ];
+    turn = 1;
+    winner= null;
+    render();
+}
+
+// Visualize all state in the DOM
+function render() {
+    renderBoard();
+    renderMessage();
+    //Hide/show UI elements ()
+    renderControls();
+}
+
+function renderBoard() {
+    board.forEach(function(colArr, colIdx) {
+        //Iterate over the cells in the cur column (colArr)
+        colArr.forEach(function(cellVal, rowIdx){
+            const cellId = `c${colIdx}r${rowIdx}`;
+            const cellEl = document.getElementById(cellId);
+            cellEl.style.backgroundColor =     
+        });
+    }); 
+}
+
+function renderMessage() {
+
+}
+
+function renderControls() {
+
+}
